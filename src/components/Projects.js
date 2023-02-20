d0066afad66de1125939d6ee4feb54968475d3f9
Brandon Clark <brandonclark427@yahoo.com>
2023-02-20 14:35:55 -0500

    Updated images
    Updated images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,9 @@ import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import pacman from "../assets/img/pacman.png";
 import todoList from "../assets/img/todolist.png";
+import dallClone from "../assets/img/dalle-clone.png";
 import projImg3 from "../assets/img/project-img3.png";
+import recipeGenerator from "../assets/img/recipe-generator.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -26,14 +28,15 @@ export const Projects = () => {
         imgUrl: projImg3,
         },
         {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: pacman,
+        title: "Dall-E 2.0 Clone",
+        description: "Fully functional clone of Dall-E 2.0 built with MERN stack.",
+
+        imgUrl: dallClone,
         },
         {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: todoList,
+        title: "Random Recipe Generator",
+        description: "Using tried and true recipes that my wife and I love, this makes meal planning SO much easier by giving us the name of the recipe as well as the ingredients and measurements!",
+        imgUrl: recipeGenerator,
         },
         {
         title: "Business Startup",
@@ -51,7 +54,7 @@ export const Projects = () => {
                 {({ isVisible }) =>
                     <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                         <h2>Projects</h2>
-                        <p>These are various projects I've worked on of varying degrees of difficulty! This website is TECHNICALLY one of them, but these are other things I've done such as a Pac-Man clone, a virtual keyboard, a to-do list, and more!</p>
+                        <p>These are various projects I've worked on of varying degrees of difficulty! This website is TECHNICALLY one of them, but these are other things I've done such as a Pac-Man clone, a virtual keyboard, a to-do list, and more! If you want to see the code for all of these, please feel free to visit by GitHub which is linked at the top and bottom of this page!</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
                         <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                             <Nav.Item>
@@ -80,10 +83,10 @@ export const Projects = () => {
                                 </Row>
                             </Tab.Pane>
                             <Tab.Pane eventKey="second">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                <p>More projects to come!</p>
                             </Tab.Pane>
                             <Tab.Pane eventKey="third">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                <p>More projects to come!</p>
                             </Tab.Pane>
                             </Tab.Content>
                         </Tab.Container>
